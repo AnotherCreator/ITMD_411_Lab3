14a96bf209dbbfb6d258bec7eb81d49c3a038180
Josh <jreginaldo@protonmail.com>
2022-02-25 14:27:22 -0600

    Change project desc. and formatting

--- src/BankRecordsTest.java
+++ src/BankRecordsTest.java
@@ -2,20 +2,20 @@
 Josh Reginaldo
 ITMD-411
 
-Lab 2 - Bank Record Generations
+Lab 3 - Bank Record Analytics
 
 A bank has received a .csv file containing raw data of various clients.
 
-Each row will be stored in an ArrayList as string arrays
-From there, iterate through the entire ArrayList and through each string array
-Create a new BankRecord object per string array and populate each field appropriately using setters
+Using Lab 2 as the foundation of the code, now implement the use of the java.util.comparator utility class to
+create a data analysis based of the records in the .csv file.
 
-Display the first 25 entries in a column-type format
+- Average income for males vs. females
+- Number of females with a mortgage and savings account
+- Number of males with both a car and 1 child per location
 */
 
 public class BankRecordsTest {
     public static void main(String[] args) {
-
         // Create a new obj and parse / cache / print .csv data
         BankRecords records = new BankRecords();
         records.readClientData();
