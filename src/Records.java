a002d9ef61a5f5ce16394ff9288939b990c33e06
Josh <jreginaldo@protonmail.com>
2022-02-28 12:24:31 -0600

    Create initial Records class

--- src/Records.java
+++ src/Records.java
@@ -1,3 +1,19 @@
-public class Records extends BankRecords {
+// Call the comparator classes here
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Records extends BankRecords {
+    static FileWriter writer = null;
+
+    public Records() {
+        try {
+            writer = new FileWriter("bankrecords.txt");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    Records bankRecords = new Records();
 }
