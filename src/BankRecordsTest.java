2a6d0f3afbb2f44aa18e9418baee38516838337d
Josh <jreginaldo@protonmail.com>
2022-02-28 12:24:15 -0600

    Add lab description

--- src/BankRecordsTest.java
+++ src/BankRecordsTest.java
@@ -9,6 +9,8 @@ A bank has received a .csv file containing raw data of various clients.
 Using Lab 2 as the foundation of the code, now implement the use of the java.util.comparator utility class to
 create a data analysis based of the records in the .csv file.
 
+Records.java will print out to the console and print out to a .txt file
+
 - Average income for males vs. females
 - Number of females with a mortgage and savings account
 - Number of males with both a car and 1 child per location
