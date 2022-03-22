4877786c4aa287f8696a68df0d3f2fdd73ada11d
Josh <jreginaldo@protonmail.com>
2022-03-22 14:52:51 -0500

    Add another main and functions to handle averages

--- src/Records.java
+++ src/Records.java
@@ -2,18 +2,49 @@
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Records extends BankRecords {
     static FileWriter writer = null;
 
+    // Create a new .txt file with the required analytics
     public Records() {
         try {
             writer = new FileWriter("bankrecords.txt");
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    Records bankRecords = new Records();
+    public static void main(String[] args) {
+
+    }
+
+    private static void clientAvgAge() {
+
+    }
+
+    private static void clientAvgSex() {
+
+    }
+
+    private static void clientAvgRegion() {
+
+    }
+
+    private static void clientAvgIncome() {
+
+    }
+
+    private static void clientAvgMarriage() {
+
+    }
+
+    private static void clientAvgChild() {
+
+    }
+
+    private static void clientAvgCar() {
+
+    }
 }
